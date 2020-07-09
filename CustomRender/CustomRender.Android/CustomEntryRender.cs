67143d260568c3b3a59f5425ba5155d56fa9cd09
Vitor <vitorleonel100@hotmail.com>
2020-07-09 15:01:39 -0300

    up

---
+++ CustomRender/CustomRender.Android/CustomEntryRender.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using CustomRender;
+using CustomRender.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRender))]
+namespace CustomRender.Droid
+{
+    public class CustomEntryRender : EntryRenderer
+    {
+
+        private Android.Graphics.Color BorderColor = Android.Graphics.Color.Black;
+
+        private int BorderWidth = 1;
+
+        private Thickness TextPadding = new Thickness(0);
+
+        private CustomEntry CustomEntry;
+
+        public CustomEntryRender(Context context) : base(context) 
+        { 
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            BorderColor = CustomEntry.BorderColor.ToAndroid();
+
+            if (CustomEntry.BorderWidth != 0)
+            {
+                BorderWidth = CustomEntry.BorderWidth;
+            }
+
+            TextPadding = CustomEntry.TextPadding;
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            CustomEntry = e.NewElement as CustomEntry;
+        }
+
+        protected override void OnLayout(bool changed, int l, int t, int r, int b)
+        {
+            base.OnLayout(changed, l, t, r, b);
+
+            if (CustomEntry != null)
+            {
+                ChangeCustomEntryProperties(CustomEntry);
+
+                Control.Gravity = GravityFlags.CenterVertical;
+
+                int left = Convert.ToInt32(TextPadding.Left);
+                int right = Convert.ToInt32(TextPadding.Right);
+                int top = Convert.ToInt32(TextPadding.Top);
+                int bottom = Convert.ToInt32(TextPadding.Bottom);
+
+                Control.SetPadding(left, top, right, bottom);
+
+                IntPtr IntPtrtextViewClass = JNIEnv.FindClass(typeof(TextView));
+                IntPtr mCursorDrawableResProperty = JNIEnv.GetFieldID(IntPtrtextViewClass, "mCursorDrawableRes", "I");
+
+                JNIEnv.SetField(Control.Handle, mCursorDrawableResProperty, Resource.Drawable.my_cursor);
+            }
+        }
+
+        private void ChangeCustomEntryProperties(CustomEntry entry)
+        {
+            //var customEntry = entry;
+
+            var nativeEditText = (EditText)Control;
+            var shape = new ShapeDrawable(new Android.Graphics.Drawables.Shapes.RectShape());
+            shape.Paint.Color = BorderColor;
+            shape.Paint.SetStyle(Paint.Style.Stroke);
+            nativeEditText.Background = shape;
+            GradientDrawable gd = new GradientDrawable();
+            //gd.SetColor(Android.Graphics.Color.White);
+            gd.SetStroke(BorderWidth, BorderColor);
+            nativeEditText.SetBackground(gd);
+        }
+
+    }
+}
---
+++ CustomRender/CustomRender.Android/obj/Debug/90/android/src/crc640ba03f288908ff23/CustomEntryRender.java
@@ -0,0 +1,63 @@
+package crc640ba03f288908ff23;
+
+
+public class CustomEntryRender
+	extends crc643f46942d9dd1fff9.EntryRenderer
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onLayout:(ZIIII)V:GetOnLayout_ZIIIIHandler\n" +
+			"";
+		mono.android.Runtime.register ("CustomRender.Droid.CustomEntryRender, CustomRender.Android", CustomEntryRender.class, __md_methods);
+	}
+
+
+	public CustomEntryRender (android.content.Context p0, android.util.AttributeSet p1, int p2)
+	{
+		super (p0, p1, p2);
+		if (getClass () == CustomEntryRender.class)
+			mono.android.TypeManager.Activate ("CustomRender.Droid.CustomEntryRender, CustomRender.Android", "Android.Content.Context, Mono.Android:Android.Util.IAttributeSet, Mono.Android:System.Int32, mscorlib", this, new java.lang.Object[] { p0, p1, p2 });
+	}
+
+
+	public CustomEntryRender (android.content.Context p0, android.util.AttributeSet p1)
+	{
+		super (p0, p1);
+		if (getClass () == CustomEntryRender.class)
+			mono.android.TypeManager.Activate ("CustomRender.Droid.CustomEntryRender, CustomRender.Android", "Android.Content.Context, Mono.Android:Android.Util.IAttributeSet, Mono.Android", this, new java.lang.Object[] { p0, p1 });
+	}
+
+
+	public CustomEntryRender (android.content.Context p0)
+	{
+		super (p0);
+		if (getClass () == CustomEntryRender.class)
+			mono.android.TypeManager.Activate ("CustomRender.Droid.CustomEntryRender, CustomRender.Android", "Android.Content.Context, Mono.Android", this, new java.lang.Object[] { p0 });
+	}
+
+
+	public void onLayout (boolean p0, int p1, int p2, int p3, int p4)
+	{
+		n_onLayout (p0, p1, p2, p3, p4);
+	}
+
+	private native void n_onLayout (boolean p0, int p1, int p2, int p3, int p4);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
